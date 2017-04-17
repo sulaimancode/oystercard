960fd32a1cc721c0dd33b499ae5221932e557f63
Nigel Said <nigelsaid@gmail.com>
2017-04-17 16:50:49 +0100

    adds #deduct

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,18 +7,21 @@ describe Oystercard do
     expect(subject.balance).to eq (0)
   end
 
-  it "should top up balance by £20" do
-    expect{ subject.top_up (20) }.to change{ subject.balance }.by (20)
-  end
-
   it "should have a default limit of £90" do
     expect(subject.card_limit).to eq (90)
+  end
+
+  it "should top up balance by £20" do
+    expect{ subject.top_up (20) }.to change{ subject.balance }.by (20)
   end
 
   it "should raise an error if card limit is exceeded" do
     expect{ subject.top_up (100) }.to raise_error "Card limit of £#{subject.card_limit} exceeded"
   end
 
-
+  it "should deduct balance by £25" do
+    subject.top_up(50)
+    expect{ subject.deduct (25)}.to change{ subject.balance}.by (-25)
+  end
 
 end
